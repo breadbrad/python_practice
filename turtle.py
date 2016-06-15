e5b1b19fcb66a49d85e31b7448171a1d1d0b1809
breadbrad <yimdonghoon@gmail.com>
2016-06-15 02:29:59 -0500

    refactoring...

--- turtle.py
+++ turtle.py
@@ -1,3 +1,4 @@
+from swampy.TurtleWorld import *
 from TurtleWorld import *
 
 world = TurtleWorld()
@@ -22,4 +23,35 @@ def circle(t, r):
 	n = 50 
 	length = circumference / n
 	polygon(t, n, length)
+
+## refactoring -> rearrangingn a program to improve function interfaces
+"""triple-quoted string --> for documentation 
+"""
+def polyline (t, n, length, angle):
+	for i in range(n):
+		fd(t, step_length)
+		lt(t, step_angle)
+
+def polygon(t, n, length):
+	angle = 360.0 / n
+	polyline(t, n, length, angle)
+
+def arc(t, r, angle):
+	arc_length = 2 * math.pi * r * angle
+	n = int (arc_length / 3) + 1 
+	step_length = arc_length / n
+	step_angle = float(angle) / n
+	polyline(t, n, step_length, step_angle)
+
+def circle(t, r):
+	arc(t, r, 360)
+
+
+
+
+
+
+
+
+
 
