4099649fc14c26f188ad56c4f29df0db6c162933
breadbrad <yimdonghoon@gmail.com>
2016-06-17 21:36:22 -0500

    map, filter..

---
+++ map, filter, reduce.py
@@ -0,0 +1,63 @@
+def capitalize_all(t):
+	res = []
+	for s in t:
+		res.append(s.capitalize())
+	return res 
+
+
+
+
+## filter example 
+
+def only_upper(t):
+	res = []
+	for s in t:
+		if s.isupper():
+			res.append(s)
+	return res 
+
+
+
+lst.pop(index)
+
+del lst[index]
+
+lst.remove(element)
+
+
+list(lst)
+
+split(lst)
+
+str.split(delimiter)
+
+#two lists are two different objects 
+
+# the association of a variable with an object is a reference 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
